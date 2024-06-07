59e1b97781501e282363b5db97b352821b522bb0
esteban mazo <estevan-mazo@hotmail.com>
2024-06-07 12:40:29 -0500

    Se hace instalacion y se accionan motores

---
+++ main.py
@@ -0,0 +1,36 @@
+from machine import Pin, PWM
+from time import sleep
+
+pwm1_pin = Pin(32, Pin.OUT)
+pwm2_pin = Pin(33, Pin.OUT)
+pwm1 = PWM(pwm1_pin)
+pwm2 = PWM(pwm2_pin)
+pwm1.freq(1000)
+pwm2.freq(1000)
+pwm1.duty(896)
+pwm2.duty(896)
+
+Motor1A = 14
+Motor1B = 27
+Motor2C = 26
+Motor2D = 25
+
+motor1A = Pin(Motor1A, Pin.OUT)
+motor1B = Pin(Motor1B, Pin.OUT)
+motor2C = Pin(Motor2C, Pin.OUT)
+motor2D = Pin(Motor2D, Pin.OUT)
+
+
+motor1A.value(1)
+motor1B.value(0)
+motor2C.value(1)
+motor2D.value(0)
+sleep(3)
+# Stop the motor after the delay
+motor1A.value(0)
+motor1B.value(1)
+motor2C.value(0)
+motor2D.value(1)
+sleep(3)
+pwm1.deinit()
+pwm2.deinit()
